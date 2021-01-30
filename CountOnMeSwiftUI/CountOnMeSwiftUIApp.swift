e857461ac1950043d6eb43b4c7f4efbff3a8a393
Ruzel Mickael <mickael.ruzel@gmail.com>
2021-01-30 17:49:25 +0100

    Initial Commit

---
+++ CountOnMeSwiftUI/CountOnMeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountOnMeSwiftUIApp.swift
+//  CountOnMeSwiftUI
+//
+//  Created by mickael ruzel on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountOnMeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
