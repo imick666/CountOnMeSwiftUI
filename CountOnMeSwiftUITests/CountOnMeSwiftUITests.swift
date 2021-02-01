34f43c0a9f208d38b9375f4b15d988ddf059c790
Ruzel Mickael <mickael.ruzel@gmail.com>
2021-02-01 13:28:49 +0100

    test (calculatorModel) write first test

--- CountOnMeSwiftUITests/CountOnMeSwiftUITests.swift
+++ CountOnMeSwiftUITests/CountOnMeSwiftUITests.swift
@@ -9,25 +9,35 @@ import XCTest
 @testable import CountOnMeSwiftUI
 
 class CountOnMeSwiftUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var env: CalculatorModel?
+    
+    override func setUp() {
+        env = CalculatorModel()
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        env = nil
+        super.tearDown()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testTwoPlusTwo() {
+        env?.pressedButton(.two)
+        env?.pressedButton(.plus)
+        env?.pressedButton(.two)
+        env?.pressedButton(.equal)
+        
+        XCTAssertEqual(env?.display, "4,0")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testTwoMultiplyByThree() {
+        env?.pressedButton(.two)
+        env?.pressedButton(.multiply)
+        env?.pressedButton(.three)
+        env?.pressedButton(.equal)
+        
+        XCTAssertEqual(env?.display, "6,0")
     }
 
 }
